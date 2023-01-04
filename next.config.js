8ddf4c9fe2c03e933adc234114d571f40275219f
ryuta-yamamura <longtaishancun554@gmail.com>
2023-01-04 23:14:08 +0900

    Stripeで決済機能を追加(英語表記は開発途中)

--- next.config.js
+++ next.config.js
@@ -6,6 +6,10 @@ const nextConfig = {
     images: {
       domains: ["links.papareact.com","fakestoreapi.com"]
     },
+
+    env: {
+      stripe_public_key: process.env.STRIPE_PUBLIC_KEY,
+    },
   }
   
   module.exports = nextConfig
--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -5,12 +5,33 @@ import CheckoutProduct from '../components/CheckoutProduct'
 import { useSelector } from 'react-redux'
 import { selectItems, selectTotal } from '../slices/basketSlice'
 import { useSession } from 'next-auth/react'
+import { loadStripe } from '@stripe/stripe-js'
+import axios from 'axios'
+
+const stripePromise = loadStripe(process.env.stripe_public_key)
 
 function checkout() {
     const items = useSelector(selectItems);
     const total = useSelector(selectTotal);
     const { data: session, status } = useSession()
     const loading = status === "loading"
+    const createCheckoutSession = async () => {
+        const stripe = await stripePromise;
+
+        // Call the checkout to create a checkout session...
+        const checkoutSession = await axios.post("/api/create-checkout-session", {
+            items: items,
+            email: session.user.email,
+        });
+
+        // redirect user/customer to Stripe Checkout
+        const result = await stripe.redirectToCheckout({
+            sessionId: checkoutSession.data.id
+        });
+
+        if (result.error) alart(result.error.message);
+    };
+
     return (
         <div className='bg-gray-100'>
             <Header />
@@ -53,18 +74,21 @@ function checkout() {
                 <div className='flex flex-col bg-white p-10 shadow-md'>
                     {items.length > 0 && (
                         <>
-                            <h2>合計金額 ({items.length} 個)
+                            <h2 className='whitespace-nowrap'>
+                                合計金額 ({items.length} 個):
                             <span className='font-bold'>
                                 <p>¥{total}</p>
                             </span>
                             </h2>
 
                             <button 
+                            role="link"
+                            onClick={createCheckoutSession}
                             disabled={!session}
                             className={`button mt-2 ${!session && 'from-gray-300 to-gray-500 border-gray-200 text-gray-300 cursor-not-allowed'}`}>
                                 {!session ? 
                                   "購入手続きに進むためには、サインインしてください。"
-                                : "購入手続きに進む"
+                                : "購入手続きへ"
                                 }
                             </button>
                         </>
