fa4f053e8ea71d5efa01888619455819c35a8172
ryuta-yamamura <longtaishancun554@gmail.com>
2023-01-09 13:47:53 +0900

    firebaseのproductキーなどを環境変数として設定変更

---
+++ firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+	apiKey: process.env.FIREBASE_APIKEY,
+	authDomain: process.env.FIREBASE_AUTHDOMAIN,
+	projectId: process.env.FIREBASE_PROJECTID,
+	storageBucket: process.env.FIREBASE_STORAGEBACKET,
+	messagingSenderId: process.env.FIREBASE_MESSAGINGSENDERID,
+	appId: process.env.FIREBASE_APPID,
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export default db;
--- src/pages/api/webhook.js
+++ src/pages/api/webhook.js
@@ -1,72 +1,82 @@
-import {buffer } from 'micro';
-import * as admin from 'firebase-admin';
+import { buffer } from "micro";
+import * as admin from "firebase-admin";
 
 // 安全性のあるFIREBASEバックエンドの接続
-const serviceAccount = require('../../../permissions.json');
+const serviceAccount = {
+	type: process.env.FIREBASE_ADMIN_TYPE,
+	project_id: process.env.FIREBASE_ADMIN_PROJECT_ID,
+	private_key_id: process.env.FIREBASE_ADMIN_PRIVATE_KEY_ID,
+	private_key: process.env.FIREBASE_ADMIN_PRIVATE_KEY,
+	client_email: process.env.FIREBASE_ADMIN_CLIENT_EMAIL,
+	client_id: process.env.FIREBASE_ADMIN_CLIENT_ID,
+	auth_uri: process.env.FIREBASE_ADMIN_AUTH_URI,
+	token_uri: process.env.FIREBASE_ADMIN_TOKEN_URI,
+	auth_provider_x509_cert_url: process.env.FIREBASE_ADMIN_AUTH_PROVIDER,
+	client_x509_cert_url: process.env.FIREBASE_ADMIN_CLIENT,
+};
+
 const app = !admin.apps.length
-? admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount)
-  })
-: admin.app();
+	? admin.initializeApp({
+			credential: admin.credential.cert(serviceAccount),
+	  })
+	: admin.app();
 
 // STRIPEへの接続を確立
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 const endpointSecret = process.env.STRIPE_SIGNING_SECRET;
 
 const fulfillOrder = async (session) => {
-    // console.log('Fullfilling order', session)
+	// console.log('Fullfilling order', session)
 
-    return app
-        .firestore()
-        .collection('users')
-        .doc(session.metadata.email)
-        .collection('orders').doc(session.id).set({
-            // USドルの場合は割る100を追加すること
-            // amount:session.amount_total / 100,
-            amount:session.amount_total,
-            amount_shipping: session.total_details.amount_shipping,
-            images: JSON.parse(session.metadata.images),
-            timestamp: admin.firestore.FieldValue.serverTimestamp(),
-        })
-        .then(() =>{
-            console.log(`SUCCESS: Order ${session.id} had been added to the DB`);
-        });
-}
-
-
+	return app
+		.firestore()
+		.collection("users")
+		.doc(session.metadata.email)
+		.collection("orders")
+		.doc(session.id)
+		.set({
+			// USドルの場合は割る100を追加すること
+			// amount:session.amount_total / 100,
+			amount: session.amount_total,
+			amount_shipping: session.total_details.amount_shipping,
+			images: JSON.parse(session.metadata.images),
+			timestamp: admin.firestore.FieldValue.serverTimestamp(),
+		})
+		.then(() => {
+			console.log(`SUCCESS: Order ${session.id} had been added to the DB`);
+		});
+};
 
 export default async (req, res) => {
-    if (req.method === 'POST') {
-        const requestBuffer = await buffer(req);
-        const payload = requestBuffer.toString();
-        const sig = req.headers["stripe-signature"];
+	if (req.method === "POST") {
+		const requestBuffer = await buffer(req);
+		const payload = requestBuffer.toString();
+		const sig = req.headers["stripe-signature"];
 
-        let event;
+		let event;
 
-        // Verify that the EVENT posted came from stripe
-        try {
-            event = stripe.webhooks.constructEvent(payload, sig, endpointSecret);
-        } catch (err) {
-            return res.status(400).send(`Webhook error: ${err.message}`)
-        }
-        // Handle the checkout.session.completed event
-        if(event.type === 'checkout.session.completed') {
-            const session = event.data.object;
+		// Verify that the EVENT posted came from stripe
+		try {
+			event = stripe.webhooks.constructEvent(payload, sig, endpointSecret);
+		} catch (err) {
+			return res.status(400).send(`Webhook error: ${err.message}`);
+		}
+		// Handle the checkout.session.completed event
+		if (event.type === "checkout.session.completed") {
+			const session = event.data.object;
 
-            // fullfill the order...
-            return fulfillOrder(session)
-            .then(() => res.status(200))
-            .catch((err) => res.status(400).send(`Webhook Error: ${err.message}`));
-
-
-        }
-    }
+			// fullfill the order...
+			return fulfillOrder(session)
+				.then(() => res.status(200))
+				.catch((err) => res.status(400).send(`Webhook Error: ${err.message}`));
+		}
+	}
 };
 
 export const config = {
-    api: {
-        bodyParser: false,
-        externalResolver: true
-    }
-}
+	api: {
+		bodyParser: false,
+		externalResolver: true,
+	},
+};
